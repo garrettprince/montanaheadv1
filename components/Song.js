fd033b93757f893dc3bb11a17d722c5ee590566b
garrettprince <89549339+garrettprince@users.noreply.github.com>
2022-06-15 21:34:24 -0700

    added basic play skip controls

--- components/Song.js
+++ components/Song.js
@@ -1,11 +1,19 @@
 import React from 'react'
+import { PlayArrow, SkipNext, SkipPrevious, Pause } from '@mui/icons-material'
 
 function Song() {
     return (
-        <div>
-            Test
-        </div>
-    )
+      <div className="flex">
+        <main className="flex-col m-auto">
+          <p className="flex">Test</p>
+          <div className='flex'>
+            <SkipPrevious className="flex m-auto h-10 w-10" />
+            <PlayArrow className="flex m-auto h-14 w-14 mx-3" />
+            <SkipNext className="flex m-auto h-10 w-10" />
+          </div>
+        </main>
+      </div>
+    );
 }
 
 export default Song
